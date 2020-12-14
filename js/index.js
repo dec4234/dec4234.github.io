f07fb24fcca8295ea4d2c8af01970116c097ca49
dec4234 <decarround04@gmail.com>
2020-12-14 14:37:19 -0500

    Updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-
+function toggleList() {
+    var x = document.getElementById("demoAcc");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className = x.previousElementSibling.className.replace(" w3-green", "");
+    }
+}
